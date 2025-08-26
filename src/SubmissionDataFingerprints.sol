ec0118383f57dc462da2c7b00a3a9da3bfff8fc5
chababa <chababa@tutanota.com>
2025-08-26 16:10:28 +0800

    test: uint test

--- src/SubmissionDataFingerprints.sol
+++ src/SubmissionDataFingerprints.sol
@@ -74,13 +74,17 @@ contract SubmissionDataFingerprints is Ownable {
         @param user the address to query
         @param submissionID the submission id to query
         @param page the page to query, starts from 0
-        @param size the size of a page
+        @param size the size of a page, MUST be larger than 0
         @return found if the record is found
         @return record the record found
         @return end if to the end
     */
     function getRecord(address user, uint256 submissionID, uint256 page, uint256 size) public view returns (bool found, Record memory record, bool end) {
         Record[] storage records = storedRecords[user];
+        if (records.length == 0) {
+            return (false, Record(0,bytes32(0)), true);
+        }
+
         uint256 startIndex = page * size;
         uint256 endIndex = startIndex + size - 1;
         end = false;
@@ -91,7 +95,7 @@ contract SubmissionDataFingerprints is Ownable {
 
         found = false;
         record = Record(0,bytes32(0));
-        for (uint i = 0; i < records.length; i++) {
+        for (uint i = startIndex; i <= endIndex; i++) {
             if (records[i].submissionID == submissionID) {
                 found = true;
                 record = records[i];
@@ -109,6 +113,10 @@ contract SubmissionDataFingerprints is Ownable {
     */
     function getRecords(address user, uint256 page, uint256 size) public view returns (Record[] memory rets, bool end) {
         Record[] storage records = storedRecords[user];
+        if (records.length == 0) {
+            return (new Record[](0), true);
+        }
+
         uint256 startIndex = page * size;
         uint256 endIndex = startIndex + size - 1;
         end = false;
--- test/SubmissionDataFingerprints.t.sol
+++ test/SubmissionDataFingerprints.t.sol
@@ -7,7 +7,152 @@ import {SubmissionDataFingerprints} from "../src/SubmissionDataFingerprints.sol"
 contract SubmissionDataFingerprintsTest is Test {
     SubmissionDataFingerprints public fingerprints;
 
+    address internal owner;
+    address internal submitter;
+
+    uint256 internal ownerKey;
+    uint256 internal submitterKey;
+
     function setUp() public {
-        fingerprints = new SubmissionDataFingerprints();
+        (owner, ownerKey) = makeAddrAndKey("owner");
+        (submitter, submitterKey) = makeAddrAndKey("submitter");
+
+        fingerprints = new SubmissionDataFingerprints(owner, submitter);
+    }
+
+    function test_submit_with_notSubmitter() public {
+        (address hacker, ) = makeAddrAndKey("hacker");
+        SubmissionDataFingerprints.Record memory record = SubmissionDataFingerprints.Record(0, bytes32(0));
+        vm.prank(hacker);
+        vm.expectRevert(abi.encodeWithSelector(SubmissionDataFingerprints.NotSubmitter.selector, hacker));
+        fingerprints.submit(hacker, record);
+    }
+
+    function test_submit_with_submitter() public {
+        SubmissionDataFingerprints.Record memory record = SubmissionDataFingerprints.Record(0, bytes32(0));
+        vm.prank(submitter);
+        fingerprints.submit(submitter, record);
+    }
+
+    function test_batchSubmit_with_notSubmitter() public {
+        (address hacker, ) = makeAddrAndKey("hacker");
+        vm.prank(hacker);
+        vm.expectRevert(abi.encodeWithSelector(SubmissionDataFingerprints.NotSubmitter.selector, hacker));
+        address[] memory users = new address[](1);
+        users[0] = hacker;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](1);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        fingerprints.batchSubmit(users, records);
+    }
+
+    function test_batchSubmit_with_parameterLengthNotEqual() public {
+        vm.prank(submitter);
+        address[] memory users = new address[](1);
+        users[0] = submitter;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](2);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        vm.expectRevert(abi.encodeWithSelector(SubmissionDataFingerprints.ParameterLengthNotEqual.selector, users.length, records.length));
+        fingerprints.batchSubmit(users, records);
+    }
+
+    function test_batchSubmit_with_submitter() public {
+        vm.prank(submitter);
+        address[] memory users = new address[](1);
+        users[0] = submitter;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](1);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        fingerprints.batchSubmit(users, records);
+    }
+
+    function test_getRecordCount_with_userNotFound() public view {
+        uint256 count = fingerprints.getRecordCount(submitter);
+        vm.assertEq(count, 0);
+    }
+
+    function test_getRecordCount() public {
+        vm.prank(submitter);
+        address[] memory users = new address[](1);
+        users[0] = submitter;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](1);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        fingerprints.batchSubmit(users, records);
+
+        uint256 count = fingerprints.getRecordCount(submitter);
+        vm.assertEq(count, 1);
+    }
+
+    function test_getRecord_with_userNotFound() public view {
+        (bool found, SubmissionDataFingerprints.Record memory record, bool end) = fingerprints.getRecord(submitter, 0, 0, 1000);
+        vm.assertEq(found, false);
+        vm.assertEq(end, true);
+        vm.assertEq(record.submissionID, 0);
+    }
+
+    function test_getRecord_with_recordNotInPage() public {
+        vm.prank(submitter);
+        address[] memory users = new address[](2);
+        users[0] = submitter;
+        users[1] = submitter;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](2);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        records[1] = SubmissionDataFingerprints.Record(1, bytes32(uint256(1)));
+        fingerprints.batchSubmit(users, records);
+
+        (bool found, SubmissionDataFingerprints.Record memory record, bool end) = fingerprints.getRecord(submitter, 1, 0, 1);
+        vm.assertEq(found, false);
+        vm.assertEq(end, false);
+        vm.assertEq(record.submissionID, 0);
+    }
+
+    function test_getRecord() public {
+        vm.prank(submitter);
+        address[] memory users = new address[](2);
+        users[0] = submitter;
+        users[1] = submitter;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](2);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        records[1] = SubmissionDataFingerprints.Record(1, bytes32(uint256(1)));
+        fingerprints.batchSubmit(users, records);
+
+        (bool found, SubmissionDataFingerprints.Record memory record, bool end) = fingerprints.getRecord(submitter, 1, 1, 1);
+        vm.assertEq(found, true);
+        vm.assertEq(end, true);
+        vm.assertEq(record.submissionID, 1);
+    }
+
+    function test_getRecords_with_userNotFound() public view {
+        (SubmissionDataFingerprints.Record[] memory records, bool end) = fingerprints.getRecords(submitter, 0, 1000);
+        vm.assertEq(end, true);
+        vm.assertEq(records.length, 0);
+    }
+
+    function test_getRecords() public {
+        vm.prank(submitter);
+        address[] memory users = new address[](3);
+        users[0] = submitter;
+        users[1] = submitter;
+        users[2] = submitter;
+        SubmissionDataFingerprints.Record[] memory records = new SubmissionDataFingerprints.Record[](3);
+        records[0] = SubmissionDataFingerprints.Record(0, bytes32(0));
+        records[1] = SubmissionDataFingerprints.Record(1, bytes32(uint256(1)));
+        records[1] = SubmissionDataFingerprints.Record(2, bytes32(uint256(2)));
+        fingerprints.batchSubmit(users, records);
+
+        // all in one page
+        bool end = false;
+        (records, end) = fingerprints.getRecords(submitter, 0, 1000);
+        vm.assertEq(end, true);
+        vm.assertEq(records.length, 3);
+
+        // not in one page
+        end = false;
+        uint256 page = 0;
+        uint256 recordCount = 0;
+        while (!end) {
+            (records, end) = fingerprints.getRecords(submitter, page, 1);
+            recordCount += records.length;
+            page++;
+        }
+        vm.assertEq(recordCount, 3);
     }
 }
